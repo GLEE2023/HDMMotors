8046cb57f159e970e5c164bfe8b04346689a30b1
jufl4329-png <jufl4329@colorado.edu>
2026-07-20 09:16:20 -0600

    Update Servo.ino

--- Servo.ino
+++ Servo.ino
@@ -1,114 +1,115 @@
 #include <Servo.h>
 
-Servo myServo;
+Servo leftServo;
+Servo rightServo;
 
-const int servoPin = 3;
-const int servoStepDelayMilliseconds = 10;
+const int servoLeftPin = 4;
+const int servoRightPin = 3;
 
-int currentAngle = 0;
+// Rest positions
+const int LEFT_REST  = 180;
+const int RIGHT_REST = 0;
 
-void moveServoSmooth(int targetAngle)
+// Fire positions
+const int LEFT_FIRE  = 60;     // CCW
+const int RIGHT_FIRE = 120;   // CW
+
+int leftAngle = LEFT_REST;
+int rightAngle = RIGHT_REST;
+
+void writeServos(int left, int right)
 {
-  targetAngle = constrain(targetAngle, 0, 180);
+    leftServo.write(constrain(left, 0, 180));
+    rightServo.write(constrain(right, 0, 180));
+}
 
-  const int angleStep = 5;
-  const int movementDelay = 5;
 
-  if (targetAngle > currentAngle)
-  {
-    for (int position = currentAngle; position < targetAngle; position += angleStep)
+void moveServosSmooth(int targetLeft, int targetRight)
+{
+    const int step = 2;
+    const int wait = 10;
+
+    while (leftAngle != targetLeft || rightAngle != targetRight)
     {
-      myServo.write(position);
-      delay(movementDelay);
-    }
-  }
-  else
-  {
-    for (int position = currentAngle; position > targetAngle; position -= angleStep)
-    {
-      myServo.write(position);
-      delay(movementDelay);
-    }
-  }
+        if (leftAngle < targetLeft)
+            leftAngle += step;
+        else if (leftAngle > targetLeft)
+            leftAngle -= step;
 
-  myServo.write(targetAngle);
-  currentAngle = targetAngle;
+        if (rightAngle < targetRight)
+            rightAngle += step;
+        else if (rightAngle > targetRight)
+            rightAngle -= step;
+
+        if (abs(leftAngle - targetLeft) < step)
+            leftAngle = targetLeft;
+
+        if (abs(rightAngle - targetRight) < step)
+            rightAngle = targetRight;
+
+        writeServos(leftAngle, rightAngle);
+        delay(wait);
+    }
 }
 
 void printControls()
 {
-  Serial.println();
-  Serial.println("===== Arduino Nano Servo Controller =====");
-  Serial.println("w = reset");
-  Serial.println("e = arm");
-  Serial.println("p = fire");
-  Serial.println("=========================================");
+    Serial.println();
+    Serial.println("===== Pinball Servo Controller =====");
+    Serial.println("w = reset");
+    Serial.println("e = arm");
+    Serial.println("p = fire");
+    Serial.println("====================================");
 }
 
 void setup()
 {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  myServo.attach(servoPin, 500, 2400);
+    leftServo.attach(servoLeftPin);
+    rightServo.attach(servoRightPin);
 
-  currentAngle = 0;
-  myServo.write(currentAngle);
+    writeServos(LEFT_REST, RIGHT_REST);
 
-  delay(500);
-
-  printControls();
+    delay(500);
+    printControls();
 }
 
 void loop()
 {
-  if (Serial.available() == 0)
-  {
-    return;
-  }
+    if (!Serial.available())
+        return;
 
-  char command = Serial.read();
-  bool commandHandled = true;
+    char command = Serial.read();
 
-  switch (command)
-  {
-    case 'w':
-      moveServoSmooth(currentAngle + 180);
-      break;
+    switch (command)
+    {
+        case 'w':
+            moveServosSmooth(LEFT_REST, RIGHT_REST);
+            break;
 
-    case 'e':
-      moveServoSmooth(currentAngle - 45);
-      break;
+        case 'e':
+            moveServosSmooth(135, 45);
+            break;
 
-    case 'p':
-      moveServoSmooth(currentAngle - 90);
-      delay(100);
-      moveServoSmooth(currentAngle + 180);
-      break;
+        case 'p':
+            moveServosSmooth(LEFT_FIRE, RIGHT_FIRE);
+            delay(250);
+            moveServosSmooth(LEFT_REST, RIGHT_REST);
+            break;
 
-    case '?':
-      printControls();
-      break;
+        case '?':
+        case 'h':
+            printControls();
+            break;
 
-    case 'h':
-      printControls();
-      break;
+        case '\n':
+        case '\r':
+            return;
+    }
 
-    case '\r':
-    case '\n':
-      commandHandled = false;
-      break;
-
-    default:
-      commandHandled = false;
-      Serial.print("Unknown command: ");
-      Serial.println(command);
-      break;
-  }
-
-  if (commandHandled)
-  {
-    Serial.print("Current angle: ");
-    Serial.print(currentAngle);
-    Serial.println(" degrees");
-  }
+    Serial.print("Left: ");
+    Serial.print(leftAngle);
+    Serial.print("  Right: ");
+    Serial.println(rightAngle);
 }
